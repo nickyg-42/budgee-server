d6e32753616c79e4ebfb349a48724c50bf2e75f9
Nicholas Grimaldi <nicholasgrimaldi42@gmail.com>
2025-11-15 17:18:07 -0500

    sql files

---
+++ src/db/migrations/000001_init.down.sql
@@ -0,0 +1,4 @@
+DROP TABLE transactions;
+DROP TABLE accounts;
+DROP TABLE plaid_items;
+DROP TABLE users;
---
+++ src/db/migrations/000001_init.up.sql
@@ -0,0 +1,54 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    email TEXT UNIQUE NOT NULL,
+    username TEXT UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE plaid_items (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    item_id TEXT UNIQUE NOT NULL,
+    access_token TEXT UNIQUE NOT NULL,
+    institution_id TEXT NOT NULL,
+    institution_name TEXT,
+    cursor TEXT,
+    status text NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT now()
+);
+
+CREATE TABLE accounts (
+    id SERIAL PRIMARY KEY,
+    item_id INTEGER REFERENCES plaid_items(id) ON DELETE CASCADE,
+    account_id TEXT UNIQUE NOT NULL,
+    name TEXT NOT NULL,
+    official_name TEXT,
+    mask TEXT NOT NULL,
+    type TEXT NOT NULL,
+    subtype TEXT NOT NULL,
+    current_balance numeric(28,10),
+    available_balance numeric(28,10),
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT now()
+);
+
+CREATE TABLE transactions (
+    id SERIAL PRIMARY KEY,
+    account_id INTEGER REFERENCES accounts(id) ON DELETE CASCADE,
+    transaction_id TEXT UNIQUE NOT NULL,
+    plaid_category_id text,
+    category text,
+    type text NOT NULL,
+    name text NOT NULL,
+    merchant_name TEXT,
+    amount numeric(28,10) NOT NULL,
+    currency TEXT,
+    date DATE NOT NULL,
+    pending BOOLEAN NOT NULL,
+    account_owner text,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT now()
+);
